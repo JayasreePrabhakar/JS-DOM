b9c118a1a47e0647392de2ac05ac95311b0a2464
JayasreePrabhakar <jayasreeprabhakar06@gmail.com>
2025-06-20 10:15:06 +0530

    complete console topic

---
+++ script.js
@@ -0,0 +1,30 @@
+//let newObject = {
+  //  name: "Jayasree",
+    //display: function() {
+      //  console.log("Hello");
+    //}
+//}
+
+//newObject.display();
+
+console Object Methods
+console.log("Jello Jayasree");
+console.warn("This is a warning");
+console.error("Something went wrong");
+
+console.table([{name : "Alice" }, { name: "Bob" }]);
+
+console.time("loop");
+for (let i =0; i< 1000000; i++) { }
+console.timeEnd("loop");
+
+//eval() function - don't use in live projects
+//const x = 10;
+//const y = 20;
+//const result = eval("x + y");
+//console.log(result);
+
+//strict mode - to rectify the error
+//"use strict";
+//let a = 10;
+//b = 20;
