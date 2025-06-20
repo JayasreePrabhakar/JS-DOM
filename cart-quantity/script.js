c01a7a34f6236b5dc61e53d3a1a20fa37c91b0ae
JayasreePrabhakar <jayasreeprabhakar06@gmail.com>
2025-06-20 15:15:59 +0530

    complete cartQuantity topic

--- cart-quantity/script.js
+++ cart-quantity/script.js
@@ -9,6 +9,32 @@ if(!cartQuantity) {
 // default operator
 //cartQuantity = cartQuantity || 0;//problem
 
+const displayButton = document.getElementById("Display-Button");
+const plusOneButton = document.getElementById("plus-1");
+const plusTwoButton = document.getElementById("plus-2");
+const plusThreeButton = document.getElementById("plus-3");
+const resetButton = document.getElementById("reset-button");
+
+displayButton.addEventListener("click", () => {
+    displayQuantity();
+});
+plusOneButton.addEventListener("click", () => {
+    updateQuantity(1);
+    displayQuantity();
+});
+plusTwoButton.addEventListener("click", () => {
+    updateQuantity(2);
+    displayQuantity();
+});
+plusThreeButton.addEventListener("click", () => {
+    updateQuantity(3);
+    displayQuantity();
+});
+resetButton.addEventListener("click", () => {
+    resetCart();
+    displayQuantity();
+});
+
 function updateQuantity(value) {
     cartQuantity = cartQuantity + value;
     localStorage.setItem("cartQuantity", cartQuantity);
